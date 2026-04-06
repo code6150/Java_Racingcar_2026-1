b0335848128f10ba5800603334367b21f43a2eed
code6150 <code6150@gmail.com>
2026-04-06 15:35:35 +0900

    refactor: 차 이름 예외처리

--- src/main/java/racingcar/ExceptionManager.java
+++ src/main/java/racingcar/ExceptionManager.java
@@ -1,4 +1,10 @@
 package racingcar;
 
 public class ExceptionManager {
+    public void CarNameExcept(String name) {
+        if (name.isEmpty()) {throw new IllegalArgumentException("왜안적음");}
+        if (name.length() > 5) {throw new IllegalArgumentException("이름5자초과됨");}
+        if (name.contains(" ")) {throw new IllegalArgumentException("공백포함됨");}
+    }
+
 }
